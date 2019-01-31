8c26445afc05a8e7e6af4e7821072eac8e198684
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-31 11:02:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,12 @@
 class CashRegister
   def initialize
     @total = 0 
+    @items = []
   end
   attr_accessor :total
-  def s
+  def add_item(item, price)
+    @items.push(item)
+    @total+=price
+  end
+  
 end
