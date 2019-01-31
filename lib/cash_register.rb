90aa04b248ed0d582ac192bfb02eaa3d69efcdec
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-31 11:17:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,6 +14,8 @@ class CashRegister
       return "There is no discount to apply"
     @total = @total-@total*@discount/100
     return "After the discount the total comes to $#{@total}."
-    
+  end
+  def items
+    return @items
   end
 end
